8b99951a35bfc2fefca5fa5685a0c985d613710c
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-05-02 01:53:09 -0300

    update ex005.py
    melhoria na otimização do código

--- exercicios-gustavo-guanabara-python/ex005.py
+++ exercicios-gustavo-guanabara-python/ex005.py
@@ -3,7 +3,5 @@ Faça um programa que leia um número inteiro e mostre na tela
 o seu sucessor e seu antecessor.
 '''
 n = int(input("Número: "))
-a = n - 1
-s = n + 1
-print(f"Seu antecessor: {a}")
-print(f"Seu sucessor: {s}")
+print(f"Seu antecessor: {n - 1}\nSeu sucessor: {n + 1} ")
+
