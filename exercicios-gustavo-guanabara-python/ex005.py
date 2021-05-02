6af3cc47b0f01738a253f9947ed0dcdc31f18141
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-05-02 01:50:58 -0300

    resolução de exercícios
    resolução de exercícios

---
+++ exercicios-gustavo-guanabara-python/ex005.py
@@ -0,0 +1,9 @@
+'''
+Faça um programa que leia um número inteiro e mostre na tela
+o seu sucessor e seu antecessor.
+'''
+n = int(input("Número: "))
+a = n - 1
+s = n + 1
+print(f"Seu antecessor: {a}")
+print(f"Seu sucessor: {s}")
