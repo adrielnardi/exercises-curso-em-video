be8f7f84cee6ffcc728779d38769393db1d9ba32
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-05-02 01:33:54 -0300

    Delete exercicios-gustavo-guanabara-python directory

--- exercicios-gustavo-guanabara-python/ex002.py
+++
@@ -1,6 +0,0 @@
-'''
-Faça um programa que leia o nome de uma pessoa e mostre
-uma mensagem de boas-vindas.
-'''
-nome = input("Nome:")
-print(f'Seja bem vindo {nome} !')
--- exercicios-gustavo-guanabara-python/ex003.py
+++
@@ -1,8 +0,0 @@
-'''
-Crie um programa que leia dois números e mostre a soma entre eles.
-'''
-n1 = int(input("1º Número: "))
-n2 = int(input("2º Número: "))
-soma = n1 + n2
-print(soma)
-
