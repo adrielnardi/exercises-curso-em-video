5c170365517cb51733dc1518d240c5ddd930b095
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-05-04 00:11:54 -0300

    update ex002.py
    atualização ex002.py

--- exercicios-gustavo-guanabara-python/ex002.py
+++ exercicios-gustavo-guanabara-python/ex002.py
@@ -1,6 +1,9 @@
 '''
 Faça um programa que leia o nome de uma pessoa e mostre
 uma mensagem de boas-vindas.
+
+Make a program that reads a person's name and shows
+a welcome message.
 '''
 nome = input("Nome:")
 print(f'Seja bem vindo {nome} !')
