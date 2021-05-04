3fd904d14aa1c988068ad02a5bd5a6c7108027a6
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-05-04 00:14:16 -0300

    update ex005.py
    atualização ex005.py

--- exercicios-gustavo-guanabara-python/ex005.py
+++ exercicios-gustavo-guanabara-python/ex005.py
@@ -1,6 +1,9 @@
 '''
 Faça um programa que leia um número inteiro e mostre na tela
 o seu sucessor e seu antecessor.
+
+Make a program that reads a whole number and shows it on the screen
+his successor and his predecessor.
 '''
 n = int(input("Número: "))
 print(f"Seu antecessor: {n - 1}\nSeu sucessor: {n + 1} ")
