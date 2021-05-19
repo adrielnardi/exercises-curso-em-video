4f1851713932aeedf17dd30effbe15bcbd23237e
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-05-19 03:43:52 -0300

    ex013

---
+++ exercicios-gustavo-guanabara-python/ex013.py
@@ -0,0 +1,9 @@
+'''
+Faça um algoritmo que leia o salário de um funcionário e mostre
+seu novo salário, com 15% de aumento.
+
+Make an algorithm that reads an employee's salary and shows
+his new salary, with a 15% increase.
+'''
+salario = float(input("Salário Atual: R$ "))
+print(f"Aumento de 15%: R${round(salario + (salario * 15/100),2)}")
