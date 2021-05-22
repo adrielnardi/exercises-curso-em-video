9afdf82e1d9c0c399aee442b3e44ecef4b90067b
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-05-22 03:34:50 -0300

    primes.py

---
+++ primes.py
@@ -0,0 +1,12 @@
+def primo(n):
+    for val in range(2,n):
+        if n % val == 0:
+            return False
+    return True
+
+lista = []
+n = int(input('Exibir primos até o número: '))
+for val in range(2,n+1):
+    if(primo(val)):
+        lista.append(val)
+print(lista)
