54833b0b8936196caa6eb1de580f492f98836cd7
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-05-27 00:11:08 -0300

    ex014

---
+++ python3-curso-em-video/Mundo 01/ex014.py
@@ -0,0 +1,9 @@
+'''
+Escreva um programa que converta uma temperatura digitada
+em °C e converta para °F.
+
+Write a program that converts a typed temperature
+in ° C and convert to ° F.
+'''
+c = float(input("Temperatura °C: "))
+print(f"Temperatura ºF: {((9*c)/5)+32}")
