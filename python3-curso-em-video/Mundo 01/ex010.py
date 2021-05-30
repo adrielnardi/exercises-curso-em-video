47a5a572d9553a02631f2bbd8a7df5823130766f
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-05-30 00:09:00 -0300

    ex010

--- python3-curso-em-video/Mundo 01/ex010.py
+++ python3-curso-em-video/Mundo 01/ex010.py
@@ -7,4 +7,5 @@ in her wallet and show how many dollars she can buy.
 '''
 dolar = 5.44
 carteira = float(input("Quanto vocẽ tem na carteira: "))
-print(f'Você tem {round(carteira / dolar,2)} dólares')
+print(f'Você tem {carteira / dolar:.2f} dólares')
+
