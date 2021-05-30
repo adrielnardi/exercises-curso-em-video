2bd8842b724ff787f5337ea9df44655bb2702113
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-05-30 00:15:28 -0300

    delete ex012

--- exercicios-gustavo-guanabara-python/ex012.py
+++
@@ -1,9 +0,0 @@
-'''
-Faça um algoritmo que leia o preço de um produto e mostre
-seu novo preço, com 5% de desconto.
-
-Make an algorithm that reads the price of a product and shows
-its new price, with 5% discount.
-'''
-valor = float(input("Preço do Produto: R$ "))
-print(f"Com desconto de 5%: R${round(valor - (valor * 5/100),2)}")
