2741116a72d4929cea2d6327ae4015314fc55e2a
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-05-30 00:15:10 -0300

    delete ex014

--- exercicios-gustavo-guanabara-python/ex014.py
+++
@@ -1,9 +0,0 @@
-'''
-Escreva um programa que converta uma temperatura digitada
-em °C e converta para °F.
-
-Write a program that converts a typed temperature
-in ° C and convert to ° F.
-'''
-c = float(input("Temperatura °C: "))
-print(f"Temperatura ºF: {((9*c)/5)+32}")
