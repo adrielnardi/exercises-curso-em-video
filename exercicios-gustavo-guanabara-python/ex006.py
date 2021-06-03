cbc6f5ebc378f8c7f6fc290b0523499a3a1aac21
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-06-03 01:14:36 -0300

    ex006

--- exercicios-gustavo-guanabara-python/ex006.py
+++
@@ -1,9 +0,0 @@
-'''
-Crie um algoritmo que leia um número e mostre o seu dobro,
-triplo e raiz quadrada.
-
-Create an algorithm that reads a number and shows its double,
-triple and square root.
-'''
-n = float(input("Número: "))
-print(f"Dobro: {n*2}.\nTriplo: {n*3}.\nRaiz Quadrada: {n**(1/2)}")
