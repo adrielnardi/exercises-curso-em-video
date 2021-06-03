b2312feb2d1c78fb92b37fd4087542852e3a777f
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-06-03 01:15:05 -0300

    ex003

--- exercicios-gustavo-guanabara-python/ex003.py
+++
@@ -1,8 +0,0 @@
-'''
-Crie um programa que leia dois números e mostre a soma entre eles.
-
-Create a program that reads two numbers and shows the sum between them.
-'''
-n1 = int(input("1º Número: "))
-n2 = int(input("2º Número: "))
-print(n1 + n2)
