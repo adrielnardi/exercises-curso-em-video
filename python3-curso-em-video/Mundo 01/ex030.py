485a94dec02cfb4a9a8c31f95d29924c2aaee77b
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-06-24 14:26:30 -0300

    ex030

---
+++ python3-curso-em-video/Mundo 01/ex030.py
@@ -0,0 +1,9 @@
+'''
+Crie um programa que leia um número inteiro e mostre na tela
+se ele é PAR ou IMPAR.
+
+Create a program that reads an integer and shows it on the screen
+whether it is even or odd.
+'''
+n = int(input("Número inteiro: "))
+print("PAR") if n % 2 == 0 else print("IMPAR")
