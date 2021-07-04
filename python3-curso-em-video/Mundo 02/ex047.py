3b1dd1ac273554c5127258cdd2fc3bc557f786ab
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-07-04 15:20:57 -0300

    ex047

---
+++ python3-curso-em-video/Mundo 02/ex047.py
@@ -0,0 +1,8 @@
+'''
+Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+
+Create a program that shows on the screen all the even numbers that are in the range between 1 and 50.
+'''
+for i in range(2, 51, 2):
+    print(i, end=' ')
+print('Fim!')
