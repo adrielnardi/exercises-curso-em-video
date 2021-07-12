bf51ffca2be3d71a4376754fc59ff7513511c417
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-07-12 09:56:25 -0300

    ex057

---
+++ python3-curso-em-video/Mundo 02/ex057.py
@@ -0,0 +1,11 @@
+'''
+Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'.
+Caso esteja errado, peça a digitação novamente até ter um valor correto.
+
+Make a program that reads a person's gender, but only accepts the 'M' or 'F' values.
+If it is wrong, ask for the typing again until it has a correct value.
+'''
+sexo = str(input('Informe seu sexo [M/F]: ')).strip().upper()[0]
+while sexo not in 'MmFf':
+    sexo = str(input('Dados inválidos. Por favor, informe seu sexo: ')).strip().upper()[0]
+print(f'Sexo {sexo} registrado com sucesso!')
