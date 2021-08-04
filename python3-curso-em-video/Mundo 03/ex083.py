8abb0e55b3058e46305cc81306218ca38e197ff7
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-08-04 15:19:25 -0300

    ex083

---
+++ python3-curso-em-video/Mundo 03/ex083.py
@@ -0,0 +1,22 @@
+'''
+Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá
+analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
+
+Create a program where the user types any expression that uses parentheses. Your application must
+analyzed if the expression passed has the parentheses open and closed in the correct order.
+'''
+expr = str(input('Digite a expressão: '))
+pilha = []
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Expressão Válida!')
+else:
+    print('Expressão Não Válida!')
