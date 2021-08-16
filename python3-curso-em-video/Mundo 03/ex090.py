2d96569d87cf0f00ef20fad20d780b0c89258628
Adriel M. D'nardi <75861319+adrielnardi@users.noreply.github.com>
2021-08-16 17:32:02 -0300

    ex090

---
+++ python3-curso-em-video/Mundo 03/ex090.py
@@ -0,0 +1,18 @@
+'''
+Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
+No final, mostre o conteúdo da estrutura na tela.
+
+Make a program that reads a student's name and average, also saving the situation in a dictionary.
+At the end, show the content of the structure on the screen.
+'''
+aluno = dict()
+aluno['nome'] = str(input('Nome: '))
+aluno['média'] = float(input(f'Média de {aluno["nome"]}: '))
+if aluno['média'] >= 7:
+    aluno['situação'] = 'Aprovado'
+elif 5 <= aluno['média'] < 7:
+    aluno['situação'] = 'Recuperação'
+else:
+    aluno['situação'] = 'Reprovado'
+for k, v in aluno.items():
+    print(f'  - {k} -> {v}')
