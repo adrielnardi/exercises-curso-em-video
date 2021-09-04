4d381cc128203637cbf20485c6479569da62eace
Adriel M. D'Nardi <75861319+adrielnardi@users.noreply.github.com>
2021-09-04 01:41:38 -0300

    delete 024

--- ex024.py
+++
@@ -1,9 +0,0 @@
-'''
-Crie um programa que leia o nome de uma cidade e diga se ela
-começa ou não com o nome "SANTO".
-
-Create a program that reads the name of a city and tells you if it
-begins or not with the name "SANTO".
-'''
-cidade = str(input("Cidade: ")).strip()
-print(f"Começa com Santo? {cidade[:5].upper() == 'SANTO'}")
